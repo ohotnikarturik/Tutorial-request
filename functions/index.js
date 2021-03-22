476ecd35d5bd9dcc7cba15d7babb1b55d1735a8c
Okhotnichenko Artur <ohotnikarturik@gmail.com>
2021-03-22 14:11:14 +0200

    add firestore functions: auth, add request form handeling

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,44 @@
 const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp()
 
-// http request 1
-exports.randomNumber = functions.https.onRequest((request, response) => {
-  const number = Math.round(Math.random() * 100)
-  console.log(number)
-  response.send(number.toString())
+// auth trigger (new user signup)
+exports.newUserSignUp = functions.auth.user().onCreate((user) => {
+  // for background triggers you must return a value/promise
+  return admin.firestore().collection('users').doc(user.uid).set({
+    email: user.email,
+    upvotedOn: [],
+  })
 })
 
-// http request 2
-exports.toTheMyPortfolio = functions.https.onRequest((request, response) => {
-  response.redirect('https://artur-portfolio.netlify.app/')
+// auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete((user) => {
+  const doc = admin.firestore().collection('users').doc(user.uid)
+  return doc.delete()
 })
 
-// http callable function
-exports.sayHello = functions.https.onCall((data, context) => {
-  const name = data.name
-  return `hello ${name} :)`
+// auth trigger (user deleted)
+exports.userDeleted = functions.auth.user().onDelete((user) => {
+  const doc = admin.firestore().collection('users').doc(user.uid)
+  return doc.delete()
+})
+
+// http callable function (adding a request)
+exports.addRequest = functions.https.onCall((data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      'unauthenticated',
+      'only authenticated users can add requests'
+    )
+  }
+  if (data.text.length > 30) {
+    throw new functions.https.HttpsError(
+      'invalid-argument',
+      'request must be no more than 30 characters long'
+    )
+  }
+  return admin.firestore().collection('requests').add({
+    text: data.text,
+    upvotes: 0,
+  })
 })
